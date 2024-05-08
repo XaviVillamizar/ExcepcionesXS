7b17582c3aadf721d2b32379dc8a714034e17082
XaviVillamizar <88797841+XaviVillamizar@users.noreply.github.com>
2024-05-08 02:21:59 -0500

    Delete RuntimeExceptions/Debugger.java

--- RuntimeExceptions/Debugger.java
+++
@@ -1,7 +0,0 @@
-package Throwable.RuntimeException;
-
-class Debugger {
-   public void debug() throws InvalidStackFrameException {
-      throw new InvalidStackFrameException("El marco de pila actual es inválido");
-   }
-}
